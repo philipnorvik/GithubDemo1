692139faeba5b19002b8a1ea005a213ea9294199
01.Philip <01.philip@gmail.com>
2018-09-28 11:10:33 +0200

    initial commit

---
+++ src/githubdemo/GithubDemo.java
@@ -0,0 +1,17 @@
+/*
+ * Javautveckling 2018
+ */
+
+package githubdemo;
+
+
+public class GithubDemo {
+
+   
+    public static void main(String[] args) {
+
+        //github test
+        
+    }
+
+}
